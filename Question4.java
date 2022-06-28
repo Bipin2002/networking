efd1ad3e59d7c27f13c5c0b1de2e67b7d08c684a
Bipin Nagarkoti <94772519+Bipin2002@users.noreply.github.com>
2022-06-28 08:44:04 +0545

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,12 +1,25 @@
-public class Question4 {
+
+public class Array4 {
+
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int[] result = new int[arr.length];
-        for(int i = arr.length-1,j = 0; i>=0; i--, j++) {
-            result[j] = arr[i];
+        int [] arr = { 1, 2, 3, 4, 5 };
+        System.out.println("Array given in input is:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        for(int c: result) {
-            System.out.print(c);
+        int temp = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--)
+            arr[i] = arr[i - 1];
+
+        arr[0] = temp;
+
+        System.out.println();
+        System.out.println("Array after rotation is:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+
     }
 }
